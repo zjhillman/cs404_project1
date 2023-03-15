93939dc5856ac6ec3d72ce224b48bbe9d6e0e0fd
Zachary Hillman <zjhillman@outlook.com>
2023-03-15 14:17:22 -0500

    Server now creates threads
    server can connect to multiple clients by making threads. server timeout at 60 seconds. Server will remain open until a client types 'exit''

--- VotingServer.java
+++ VotingServer.java
@@ -13,41 +13,30 @@ public class VotingServer {
 
             //establish server
             ServerSocket server = new ServerSocket(port);
+            server.setSoTimeout(60000); // 60 seconds
             System.out.println("Established server at "+InetAddress.getLocalHost().toString());
 
-            // accept connection
-            Socket socket = server.accept();
-            System.out.println("Connection established.");
+            while (true) {
+                // accept connection
+                Socket socket = server.accept();
+                System.out.println("Connection established.");
 
-            while(true) {
                 // set input stream
                 InputStream inStream = socket.getInputStream();
-                BufferedReader input = new BufferedReader(new InputStreamReader(inStream));
+                BufferedReader clientIn = new BufferedReader(new InputStreamReader(inStream));
 
                 // set output stream
                 OutputStream outStream = socket.getOutputStream();
-                PrintWriter output = new PrintWriter(new OutputStreamWriter(outStream));
+                PrintWriter clientOut = new PrintWriter(new OutputStreamWriter(outStream));
 
-                // convert message to String
-                String message = "";
-                message = input.readLine();
-                System.out.println("I heard '"+message +"'\n");
+                Thread thread = new Thread(new VotingThread(socket, clientIn, clientOut));
+                thread.start();
 
-                if (message.equalsIgnoreCase("exit")) {
-                    // finish
-                    input.close();
-                    output.close();
-                    socket.close();
-                    break;
-                }
-
-                // echo message to client
-                output.print(message +"\n");
-                output.flush();
+                
             } // end while
 
-            System.out.println("Goodbye!");
-            server.close();
+            //System.out.println("Goodbye!");
+            //server.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- VotingThread.java
+++ VotingThread.java
@@ -1,7 +1,40 @@
 import java.net.*;
+import java.io.*;
 
-public class VotingThread {
-    public void run() {
-        
+public class VotingThread implements Runnable{
+    Socket socket;
+    BufferedReader clientInput;
+    PrintWriter clientOutput;
+
+    VotingThread(Socket socket, BufferedReader br, PrintWriter pw) {
+        this.socket = socket;
+        this.clientInput = br;
+        this.clientOutput = pw;
     }
-}
+
+    public void run() {
+        try {
+            while (true) {
+                // convert message to String
+                String message = "";
+                message = clientInput.readLine();
+                System.out.println("I heard '"+message +"'\n");
+
+                if (message.equalsIgnoreCase("exit")) {
+                    // finish
+                    clientInput.close();
+                    clientOutput.close();
+                    socket.close();
+                }
+
+                // echo message to client
+                clientOutput.print(message +"\n");
+                clientOutput.flush();
+            } // end while
+                
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // end try/catch
+        
+    } // end run
+} // end class
