eaedb85c17dc1376af6a6f0bc7716f339078c969
Zachary Hillman <zjhillman@outlook.com>
2023-03-16 13:31:02 -0500

    added greet function

--- VotingThread.java
+++ VotingThread.java
@@ -1,24 +1,12 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class VotingThread implements Runnable{
     Socket socket;
     BufferedReader inputFromClient;
     PrintWriter outputToClient;
-
-    // string menu
-    String welcome0 = "Welcome [user],\n";
-    String welcome1 = "Today's topic is\n";
-    String welcome2 = "    'Do you believe homework should be abolished?'\n";
-    String welcome3 = "Would you like to cast a vote?\n";
-    String welcome4 = "[1] I would like to cast a vote\n";
-    String welcome5 = "[2] I do not wish to vote today\n";
-    String greeting = welcome0 + welcome1 + welcome2 + welcome3 + welcome4 + welcome5 + "\n";
-
-    String option0 = "[1] Yes\n";
-    String option1 = "[2] No\n";
-    String option2 = "[3] Don't Care\n";
-    String voteOptions = welcome1 + welcome2 + option0 + option1 + option2 + "\n";
+   
 
     String post0 = "Thanks for participating in today's poll!";
     String post1 = "Please choose one of the following:";
@@ -36,25 +24,71 @@ public class VotingThread implements Runnable{
         this.outputToClient = pw;
     }
 
+    public int greet() {
+        String welcome0 = "Welcome [user],\n";
+        String welcome1 = "Today's topic is\n";
+        String welcome2 = "    'Do you believe homework should be abolished?'\n";
+        String welcome3 = "Would you like to cast a vote?\n";
+        String welcome4 = "[1] I would like to cast a vote\n";
+        String welcome5 = "[2] I do not wish to vote today\n";
+        String greeting = welcome0 + welcome1 + welcome2 + welcome3 + welcome4 + welcome5 + "\n";
+        List<String> options = new ArrayList<String>();
+        options.add("1");
+        options.add("2");
+        options.add(".");
+
+        // send greeting
+        outputToClient.print(greeting + "\n");
+        outputToClient.flush();
+
+        // get response
+        try {
+            String response = inputFromClient.readLine();
+
+            while (!options.contains(response)) {
+                outputToClient.print("Invalid response, your options are\n" + welcome4 + welcome5 + "\n");
+
+                response = inputFromClient.readLine();
+            }
+
+            return Integer.parseInt(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public void printBallot() {
+        String intro = "Today's topic is\n";
+        String topic = "    'Do you believe homework should be abolished?'\n";
+        String vote1 = "[1] Yes\n";
+        String vote2 = "[2] No\n";
+        String vote3 = "[3] Don't Care\n";
+        String poll = intro + topic + vote1 + vote2 + vote3 + "\n";
+
+        // send poll
+        outputToClient.print(poll + "\n");
+        outputToClient.flush();
+    }
+
+
+
     public void run() {
         try {
-            // send greeting
-            outputToClient.print(greeting + "\n");
-            outputToClient.flush();
+            int choice;
+            choice = greet();
+            
+            System.out.println("[client][greeting_response] " + response);
 
-            // wait for response
-            String msg = inputFromClient.readLine();
-            System.out.println("[client][greeting_response] " + msg);
-
-            switch (Integer.parseInt(msg)) {
+            switch (Integer.parseInt(response)) {
                 case 1:
-                    System.out.println("yes vote");
+                    printBallot();
                     break;
                 case 2:
-                    System.out.println("no vote");
+                    System.out.println("client does not wish to vote");
                     break; 
                 default:
-                    System.out.println("Invalid option");
+                    System.out.println("client entered an invalid option");
                     return;
             }
             
