4c4b12e9bcce4cadc37f148cff56443848e3e2fe
Zachary Hillman <zjhillman@outlook.com>
2023-03-15 23:15:25 -0500

    Update VotingClient.java
    added shutdown function, isNumeric function, updating for server thread

--- VotingClient.java
+++ VotingClient.java
@@ -2,10 +2,38 @@ import java.net.*;
 import java.io.*;
 
 public class VotingClient {
-    public static void main (String args[]) {
-        InetAddress hostName;
-        int hostPort = 12320;
+    private static InetAddress hostName;
+    private static int hostPort = 12320;
+    private static Socket socket;
+    private static BufferedReader inputFromServer;
+    private static PrintWriter outputToServer;
+    private static BufferedReader inputFromUser;
+    private static String exitCmd = ".";
 
+
+    private static void shutdown() {
+        try {
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isNumeric(String str) {
+        if (str == null) 
+            return false;
+
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+
+    public static void main (String args[]) {
         try {
             hostName = InetAddress.getByName("localhost");
 
@@ -17,46 +45,47 @@ public class VotingClient {
                 hostPort = Integer.parseInt(args[1]);
             }
 
-            Socket socket = null;
+            socket = null;
             OutputStream outStream = null;
             InputStream inStream = null;
-
 
             // establish connection to server
             socket = new Socket(hostName, hostPort);
             System.out.println("Established connection to "+socket.getInetAddress().toString());
+            
+            // set output stream 
+            outStream = socket.getOutputStream();
+            outputToServer = new PrintWriter(new OutputStreamWriter(outStream));
+
+            // set input stream
+            inStream = socket.getInputStream();
+            inputFromServer = new BufferedReader(new InputStreamReader(inStream));
+
+            // set user input
+            inputFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+            // get message from server
+            String line = "";
+            while ((line = inputFromServer.readLine()) != null && line.equals("")) {
+                System.out.println(line);
+            }
+
+            // get user input
+            String userInput = "";
+            userInput = inputFromUser.readLine();
+            System.out.println("entered "+userInput);
+            while (!isNumeric(userInput)) {
+                System.out.print("Invalid option, enter one of the options above");
+                userInput = inputFromUser.readLine();
+            }
+
+            // send message to server
+            outputToServer.print(userInput + "\n");
+            outputToServer.flush();
 
             while (true) {
-                // set output stream 
-                outStream = socket.getOutputStream();
-                PrintWriter output = new PrintWriter(new OutputStreamWriter(outStream));
-
-                // set input stream
-                inStream = socket.getInputStream();
-                BufferedReader input = new BufferedReader(new InputStreamReader(inStream));
-
-                // set user input
-                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-
-                // send a message
-                System.out.print("Please enter a message for the server: ");
-                String message = userInput.readLine();;
-                output.print(message + "\n");
-                output.flush();
-
-                // exit command
-                if (message.equalsIgnoreCase("exit")) {
-                    output.close();
-                    input.close();
-                    break;
-                }
-                // wait for response
-                String response = "";
-                response = input.readLine();
-                System.out.println(response + "\n");
+                
             } // end while
-
-            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
